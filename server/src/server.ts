08cf17efaa36f339cbebf1b59e495bdddc3dc6b3
Idil Abdikadir <idil.linkedin@gmail.com>
2026-03-16 00:33:34 +0100

    Authentication is set up end-to-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,10 @@
+import bcrypt from "bcrypt";
 import cors from "cors";
 import express from "express";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = "supersecretkey";
+
 
 const app = express();
 
@@ -13,11 +18,13 @@ type User = {
 
 const users: User[] = [];
 
+
 app.get("/", (req, res) => {
   res.send("Authentication server running");
 });
 
-app.post("/register", (req, res) => {
+
+app.post("/register", async (req, res) => {
   const { email, password } = req.body;
 
   const existingUser = users.find((u) => u.email === email);
@@ -26,11 +33,42 @@ app.post("/register", (req, res) => {
     return res.status(400).json({ message: "User already exists" });
   }
 
-  users.push({ email, password });
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  users.push({ 
+    email, 
+    password: hashedPassword });
 
   console.log("Registered users:", users);
 
   res.json({ message: "User registered successfully" });
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = users.find((u) => u.email === email);
+
+  if (!user) {
+    return res.status(400).json({ message: "Invalid email or password" });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid email or password" });
+  }
+
+  const token = jwt.sign(
+  { email: user.email },
+  JWT_SECRET,
+  { expiresIn: "1h" }
+);
+
+res.json({
+  message: "Login successful",
+  token
+});
 });
 
 const PORT = 5000;
